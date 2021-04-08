3cddbb132e1f6952682a74d458209c8778249370
Async-Clock <62951815+Async-Clock@users.noreply.github.com>
2021-04-07 21:39:51 -0500

    Added install script and dependencies

---
+++ Install/prometheus_install.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+#Assumes the required files are sitting in user's home directory
+#prometheus.yml, prometheus.conf, node_exporter.service, prometheus.service
+cd ~
+mkdir Prometheus
+cd Prometheus
+wget https://github.com/prometheus/prometheus/releases/download/v2.26.0/prometheus-2.26.0.linux-amd64.tar.gz
+tar xvzf prometheus-2.26.0.linux-amd64.tar.gz
+sudo useradd -rs /bin/false prometheus
+cd prometheus-2.26.0.linux-amd64
+sudo rm prometheus.yml
+sudo cp prometheus promtool /usr/local/bin
+
+sudo chown prometheus:prometheus /usr/local/bin/prometheus
+sudo mkdir /etc/prometheus
+cd ~
+sudo cp prometheus.yml Prometheus/prometheus-2.26.0.linux-amd64
+cd Prometheus/prometheus-2.26.0.linux-amd64
+sudo cp -R consoles/ console_libraries/ prometheus.yml /etc/prometheus
+sudo mkdir -p data/prometheus
+sudo chown -R prometheus:prometheus data/prometheus /etc/prometheus/*
+cd ~
+sudo cp prometheus.service /lib/systemd/system
+#Add test to see if nginx installed possibly, also probably not needed
+sudo apt-get install nginx
+sudo cp prometheus.conf /etc/nginx/conf.d
+sudo apt-get install apache2-utils
+cd /etc/prometheus
+sudo htpasswd -c .credentials admin
+#will get input from user
+sudo apt-get install gnutls-bin
+cd /etc/ssl
+sudo mkdir prometheus
+sudo certtool --generate-privkey --outfile prometheus-private-key.pem
+
+sudo certtool --generate-self-signed --load-privkey prometheus-private-key.pem --outfile prometheus-cert.pem
+
+cd ~/Prometheus
+wget https://github.com/prometheus/node_exporter/releases/download/v1.1.2/node_exporter-1.1.2.linux-amd64.tar.gz
+tar xvzf node_exporter-1.1.2.linux-amd64.tar.gz
+cd node_exporter-1.1.2.linux-amd64
+sudo cp node_exporter /usr/local/bin
+sudo useradd -rs /bin/false node_exporter
+sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
+cd ~
+sudo cp node_exporter.service /lib/systemd/system
+
+sudo systemctl daemon-reload
+sudo systemctl restart nginx
+sudo systemctl restart prometheus
+sudo systemctl start node_exporter
+
+#Grafana
+sudo wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+
+sudo apt-get update
+sudo apt-get install grafana
+sudo systemctl start grafana-server
+#NOTE: This will not disable anon access by default
+#localhost:3000 for Grafana, localhost:9090 for Prometheus
+
+
+
+
+
+
+
